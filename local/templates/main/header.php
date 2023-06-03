9e748bd5160d2f501660d1aa936829ad9b3094c7
Roman Dudenko <predateljohn@gmail.com>
2023-06-04 02:00:29 +0400

    first commit

---
+++ local/templates/main/header.php
@@ -0,0 +1,19 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<?$APPLICATION->ShowHead();?>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="images/favicon.604825ed.ico" type="image/x-icon">
+	<link href="<?= SITE_TEMPLATE_PATH ?>/assets/css/common.css" rel="stylesheet">
+</head>
+<body>
+<div id="panel">
+	<?$APPLICATION->ShowPanel();?>
+</div>
+
---
+++ test.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Текстовая страница");
+?><? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
